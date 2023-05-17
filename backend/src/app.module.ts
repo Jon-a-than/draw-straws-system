050a515487395a36b7d26e3d19c8ea3a6e453bd9
Jon-a-than <2140145186@qq.com>
2023-05-17 21:42:28 +0800

    📦️ build: 添加redis缓存依赖

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common'
 import { AppController } from '@/app.controller'
 import { MongooseModule } from '@nestjs/mongoose'
-
+import { CacheModule } from '@nestjs/cache-manager'
+import { ioRedisStore } from '@tirke/node-cache-manager-ioredis'
 import { DrawStrawModule } from './modules/drawStraws/drawStraws.module'
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://${process.env.MONGO_HOST ?? '127.0.0.1'}:27017`, {
-      dbName: 'drawStrawsPool'
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017', { dbName: 'drawStrawsPool' }),
+    CacheModule.register({
+      store: ioRedisStore,
+      host: 'localhost',
+      port: 6379,
+      isGlobal: true
     }),
     DrawStrawModule
   ],
