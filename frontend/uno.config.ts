eb21dd479a93a571bff78406037eb03b2af6a91a
Jon-a-than <2140145186@qq.com>
2023-05-19 20:43:03 +0800

    ➕ build: vite添加jsx支持

--- frontend/uno.config.ts
+++ frontend/uno.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerAttributifyJsx
+} from 'unocss'
 
 export default defineConfig({
+  transformers: [transformerAttributifyJsx()],
   presets: [
     presetAttributify({ prefix: 'v:' }),
     presetIcons({
