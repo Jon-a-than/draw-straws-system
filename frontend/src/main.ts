79a446e96b2bfd66fa31d413a4002b1068e1a0a8
Jon-a-than <2140145186@qq.com>
2023-05-26 20:33:10 +0800

    🚧 wip: pinia添加持久化插件

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,13 +4,14 @@ import '@/index.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from '@/layout'
 import router from '@/router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia().use(piniaPluginPersistedstate))
 app.use(router)
 
 app.mount('#app')
