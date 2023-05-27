373270747688c1f35bb5fffec72f5e7de75c2263
Jon-a-than <2140145186@qq.com>
2023-05-27 14:24:18 +0800

    ✨ feat: 允许跨域请求

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe, Logger } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(4936)
   new Logger('NestHostListen').log(`\x1B[34m${await app.getUrl()}`)
