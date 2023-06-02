f8448398886b66f95f0f42a1e6bc4376f9b970d2
Jon-a-than <2140145186@qq.com>
2023-06-02 21:52:52 +0800

    🏷️ chore: 添加unocss类型声明

--- mobile/env.d.ts
+++ mobile/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+import type { AttributifyAttributes } from '@unocss/preset-attributify'
+
+declare module 'vue' {
+  interface HTMLAttributes<_T> extends AttributifyAttributes {}
+}
